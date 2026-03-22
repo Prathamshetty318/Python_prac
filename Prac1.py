e777338ddee23262c5ea07c0218d548b8db9db97
Pratham <shettypratham73@gmail.com>
2026-03-22 10:28:04 +0530

    learned conditions based multiple conditiosn and grouby filter concepts

--- Prac1.py
+++ Prac1.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-df = pd.read_csv('student-data.csv')
+df = pd.read_csv("student-data.csv")
 
 #print(df.describe())
 
@@ -14,4 +14,10 @@ df = pd.read_csv('student-data.csv')
 
 #print(df[["age","sex","address"]])
 
-print (df[df["age"]<20])
+#print (df[df["age"]<20])
+
+#print(len(df[df["age"]<20]))
+
+#print (df["sex"].value_counts())
+
+print(df[(df["age"] > 20) & (df["sex"] == "M")])
