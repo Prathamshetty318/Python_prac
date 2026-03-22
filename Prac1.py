7c10c4d1cbd8bd0254ae8051cabfb630ca777307
Pratham <shettypratham73@gmail.com>
2026-03-22 11:56:38 +0530

    did some group by and analysis based on it

--- Prac1.py
+++ Prac1.py
@@ -2,6 +2,8 @@ import pandas as pd
 
 df = pd.read_csv("student-data.csv")
 
+#print(f"Rows found:{len(df)}")
+
 #print(df.describe())
 
 #print(df.head())
@@ -16,8 +18,30 @@ df = pd.read_csv("student-data.csv")
 
 #print (df[df["age"]<20])
 
-#print(len(df[df["age"]<20]))
+
+#print(df[(df["age"] > 20) & (df["sex"] == "M")])
+
+#print(df[df["age"]<20])
 
 #print (df["sex"].value_counts())
 
-print(df[(df["age"] > 20) & (df["sex"] == "M")])
+#print(df[(df["age"] < 20) & (df["passed"] == "yes")])
+
+#print(df["passed"].value_counts())
+
+#print(df["studytime"].mean())
+
+#print(df.groupby("sex")["studytime"].mean())
+
+
+#print(df.groupby('passed')["Walc"].mean())
+
+#print(df.groupby("passed")["studytime"].mean())
+
+#print(df.groupby("passed")["absences"].mean())
+
+#print(df.groupby(["sex","passed"])["studytime"].mean())
+
+print(df.groupby("romantic")["absences"].mean())
+print(df.groupby("Fjob")["absences"].mean())
+print(df.groupby("Mjob")["absences"].mean())
