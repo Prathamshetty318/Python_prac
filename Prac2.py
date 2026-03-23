2b87465b3c1e47c50bd4f39d6b30ed53e1659bce
Pratham <shettypratham73@gmail.com>
2026-03-24 03:21:17 +0530

    improved chart by adding meanings and visible points on both the bar

--- Prac2.py
+++ Prac2.py
@@ -7,10 +7,22 @@ df = pd.read_csv("student-data.csv")
 #plt.title("Passed vs Failed")
 #plt.show()
 
-#df.groupby('passed')['absences'].mean().plot(kind="bar")
-#plt.title("Avg Absence by pass/fail ")
-#plt.show()
-
 #df.groupby('romantic')['absences'].mean().plot(kind='bar')
 #plt.title("Avg Absence by romantic relationship")
 #plt.show()
+
+#df.groupby('passed')['studytime'].mean().plot(kind='bar')
+#plt.title("Avg Study Time by pass/fail")
+#plt.show()
+
+ax = df.groupby('passed')['absences'].mean().plot(kind="bar")
+plt.title("Avg Absence by pass/fail ")
+plt.xlabel("Results")
+plt.ylabel("Avg Absences")
+
+plt.xticks(rotation=0)
+
+for i,v in enumerate(df.groupby("passed")["absences"].mean()):
+    ax.text(i,v + 0.1,round (v,2), ha='center')
+    
+plt.show()
