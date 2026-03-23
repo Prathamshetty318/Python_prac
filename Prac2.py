00437d67c8b8b2d9828118d7ec5a443c850fd7f5
Pratham <shettypratham73@gmail.com>
2026-03-24 03:02:53 +0530

    did analysis using bar for both stds getting failed by both absence and romantic

--- Prac2.py
+++ Prac2.py
@@ -3,6 +3,14 @@ import pandas as pd
 
 df = pd.read_csv("student-data.csv")
 
-df['passed'].value_counts().plot(kind="bar")
-plt.title("Passed vs Failed")
-plt.show()
+#df['passed'].value_counts().plot(kind="bar")
+#plt.title("Passed vs Failed")
+#plt.show()
+
+#df.groupby('passed')['absences'].mean().plot(kind="bar")
+#plt.title("Avg Absence by pass/fail ")
+#plt.show()
+
+#df.groupby('romantic')['absences'].mean().plot(kind='bar')
+#plt.title("Avg Absence by romantic relationship")
+#plt.show()
